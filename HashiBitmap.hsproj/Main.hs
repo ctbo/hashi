42c02b8beec2a7bcbf2316a9ee3d0a8a2dc19429
Harald Bögeholz <bo@ct.de>
2017-04-19 22:42:21 +1000

    completed (?) compileProblem.

--- HashiBitmap.hsproj/Main.hs
+++ HashiBitmap.hsproj/Main.hs
@@ -1,7 +1,9 @@
 import Control.Monad
-import Data.List (elemIndex, find)
+import Data.List (elemIndex, find, foldl')
+import Data.Bits
+import Data.Maybe (fromJust)
 
-
+type Bitvector = Integer
 type Index = (Int, Int, Int)
 type Direction = (Int, Int, Int)
 type Problem = [(Index, Int)]
@@ -18,6 +20,8 @@ mdirections = map (.* (-1)) pdirections
 directions :: [Direction]
 directions = pdirections ++ mdirections
 
+ndirections = length directions
+npdirections = length pdirections
 
 closestNeighbor :: [Index] -> Int -> Index -> Direction -> Maybe Index
 closestNeighbor islands m i0 di = find isIsland [i0 .+ (di.*f) | f <- [1..m]]
@@ -40,23 +44,59 @@ readProblem s = do
             _    -> case elemIndex ch ".123456789abc" of
                       Just n -> Right ((l, r, c+1), (idx,n):islands)
                       _      -> Left $ "Invalid character: '" ++ [ch] ++ "' at layer " ++ show l ++ ", row " ++ show r ++ ", column " ++ show c
-                      
-
+                   
+type BridgeConfig = [Int] -- number of bridges corresponding to directions
+   
+data Island = Island { iIndex :: Index
+                     , iLabel :: Int
+                     , iBridgeVectors :: [(BridgeConfig, Bitvector)]
+                     , iFootprint :: Bitvector
+                     } deriving (Show)
+                   
 -- compileProblem :: [(Index, Int)] -> Problem
 compileProblem rawIslands = map compileIsland rawIslands
   where islandIndices = map fst rawIslands
-        bbox@(bbx,bby,bbz) = maxIndex islandIndices .+ (1,1,1)
-        diameter = maximum [bbx, bby, bbz]
-        bridgeBitOffset = bbx*bby*bbz
-        compileIsland (i, nbridges) = bc
+        bbox@(bbl,bbr,bbc) = maxIndex islandIndices .+ (1,1,1)
+        diameter = maximum [bbl, bbr, bbc]
+        bridgeBitsStart = bbl*bbr*bbc
+        bridgeBitsOffset i = bridgeBitsStart + 3 * ndirections * fromJust (elemIndex i islandIndices)
+        compileIsland (i, nbridges) = Island { iIndex = i
+                                             , iLabel = nbridges
+                                             , iBridgeVectors = bvs
+                                             , iFootprint = bitOr $ map snd bvs
+                                             }
           where neighbors = map (closestNeighbor islandIndices diameter i) directions
-                bc = bridgeConfigurations neighbors nbridges
+                bcs = bridgeConfigurations neighbors nbridges
+                bvs = map f bcs
+                f bc = (bc, bc2bv bc)
+                bc2bv bc = shift (bridges2bits bc) (bridgeBitsOffset i)    -- our bridges
+                       .|. bitOr (zipWith3 other bc neighbors [3,4,5,0,1,2]) -- block non-matching neighbors
+                       .|. bitOr (zipWith3 drawBridge bc neighbors pdirections)
+                other :: Int -> Maybe Index -> Int -> Bitvector
+                other b n d = case n of
+                                Nothing -> 0
+                                Just j -> shift (7 `xor` (shift 1 b)) $ d + bridgeBitsOffset j
+                drawBridge b n d 
+                  | b == 0 = 0
+                  | otherwise = case n of
+                                   Nothing -> 0 -- can't happen
+                                   Just ni -> bitOr $ map idxbit $ takeWhile (/= ni) [i .+ (d .* k) | k <- [0..]]
+                idxbit (l, r, c) = shift 1 $ (l*bbr+r)*bbc + c
 
-bridgeConfigurations :: [Maybe t] -> Int -> [[Int]]
+bridgeConfigurations :: [Maybe t] -> Int -> [BridgeConfig]
 bridgeConfigurations [] 0 = [[]]
 bridgeConfigurations [] _ = []
 bridgeConfigurations (Nothing:bs) n = map (0:) $ bridgeConfigurations bs n
 bridgeConfigurations (_:bs) n =  concatMap (\i -> map (i:) $ bridgeConfigurations bs (n-i)) [0..2]
+
+bridges2bits :: [Int] -> Bitvector
+bridges2bits [] = 0
+bridges2bits (b:bs) = shift (bridges2bits bs) 3 .|. shift 1 b
+
+  
+bitOr :: [Bitvector] -> Bitvector
+bitOr = foldl' (.|.) 0
+
 
 (.+) ::(Num t) => (t, t, t) -> (t, t, t) -> (t, t, t)
 (x, y, z) .+ (x', y', z') = (x+x', y+y', z+z')
