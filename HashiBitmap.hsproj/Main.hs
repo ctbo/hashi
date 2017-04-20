3530a87a0191afefc628bbdd1809e345c08b1d4e
Harald Bögeholz <bo@ct.de>
2017-04-20 21:33:07 +1000

    bugfix in CompileProblem. solve.

--- HashiBitmap.hsproj/Main.hs
+++ HashiBitmap.hsproj/Main.hs
@@ -6,7 +6,6 @@ import Data.Maybe (fromJust)
 type Bitvector = Integer
 type Index = (Int, Int, Int)
 type Direction = (Int, Int, Int)
-type Problem = [(Index, Int)]
 
 pdirections :: [Direction]
 pdirections = [ (0,0,1)
@@ -31,7 +30,7 @@ closestNeighbor islands m i0 di = find isIsland [i0 .+ (di.*f) | f <- [1..m]]
 maxIndex :: [Index] -> Index
 maxIndex = foldr1 (\(x,y,z) (x',y',z') -> (max x x', max y y', max z z'))
 
--- readProblem :: String -> Either String Problem
+readProblem :: String -> Either String Problem
 readProblem s = do
     (_, islands) <- foldM collect ((0,0,0), []) s
     return $ compileProblem islands
@@ -47,24 +46,26 @@ readProblem s = do
                    
 type BridgeConfig = [Int] -- number of bridges corresponding to directions
    
-data Island = Island { iIndex :: Index
-                     , iLabel :: Int
-                     , iBridgeVectors :: [(BridgeConfig, Bitvector)]
-                     , iFootprint :: Bitvector
-                     } deriving (Show)
-                   
--- compileProblem :: [(Index, Int)] -> Problem
+data CompiledIsland = CompiledIsland { iIndex :: Index
+                                     , iLabel :: Int
+                                     , iBridgeVectors :: [(BridgeConfig, Bitvector)]
+                                     , iFootprint :: Bitvector
+                                     } deriving (Eq, Show)
+
+type Problem = [CompiledIsland]    
+               
+compileProblem :: [(Index, Int)] -> Problem
 compileProblem rawIslands = map compileIsland rawIslands
   where islandIndices = map fst rawIslands
         bbox@(bbl,bbr,bbc) = maxIndex islandIndices .+ (1,1,1)
         diameter = maximum [bbl, bbr, bbc]
         bridgeBitsStart = bbl*bbr*bbc
         bridgeBitsOffset i = bridgeBitsStart + 3 * ndirections * fromJust (elemIndex i islandIndices)
-        compileIsland (i, nbridges) = Island { iIndex = i
-                                             , iLabel = nbridges
-                                             , iBridgeVectors = bvs
-                                             , iFootprint = bitOr $ map snd bvs
-                                             }
+        compileIsland (i, nbridges) = CompiledIsland { iIndex = i
+                                                     , iLabel = nbridges
+                                                     , iBridgeVectors = bvs
+                                                     , iFootprint = bitOr $ map snd bvs
+                                                     }
           where neighbors = map (closestNeighbor islandIndices diameter i) directions
                 bcs = bridgeConfigurations neighbors nbridges
                 bvs = map f bcs
@@ -75,7 +76,7 @@ compileProblem rawIslands = map compileIsland rawIslands
                 other :: Int -> Maybe Index -> Int -> Bitvector
                 other b n d = case n of
                                 Nothing -> 0
-                                Just j -> shift (7 `xor` (shift 1 b)) $ d + bridgeBitsOffset j
+                                Just j -> shift (7 `xor` (shift 1 b)) (3*d + bridgeBitsOffset j)
                 drawBridge b n d 
                   | b == 0 = 0
                   | otherwise = case n of
@@ -93,10 +94,53 @@ bridges2bits :: [Int] -> Bitvector
 bridges2bits [] = 0
 bridges2bits (b:bs) = shift (bridges2bits bs) 3 .|. shift 1 b
 
-  
+
+
+data SolvedIsland = SolvedIsland { sIndex :: Index
+                                 , sLabel :: Int
+                                 , sBridgeConfig :: BridgeConfig
+                                 } deriving (Eq, Show)
+
+type Solution = [SolvedIsland]
+
+solve :: Problem -> [Solution]
+solve = solve' 0 []
+
+solve' :: Bitvector -> Solution -> Problem -> [Solution]
+solve' _ sis [] = [sis]
+solve' b sis (ci:cis) = concatMap recurse next
+  where step (bc, bv) = if b .&. bv == 0
+                        then [(b .|. bv, SolvedIsland (iIndex ci) (iLabel ci) bc)]
+                        else []
+        next :: [(Bitvector, SolvedIsland)]
+        next = concatMap step (iBridgeVectors ci)
+        recurse :: (Bitvector, SolvedIsland) -> [Solution]
+        recurse (b', si) = solve' b' (si:sis) cis
+
+
+-- debugging helper ---------------------
+
+showBitvector :: Index -> Bitvector -> String
+showBitvector (bbl, bbr, bbc) b = field ++ showIslands 0 (shiftR b (bbl*bbr*bbc))
+  where field = concatMap field' [0..bbl-1]
+        field' l = concatMap (field'' l) [0..bbr-1] ++ "\n"
+        field'' l r = concatMap (field''' l r) [0..bbc-1] ++ "\n"
+        field''' l r c = if testBit b ((l*bbr+r)*bbc + c) then "+" else "."
+        showIslands :: Int -> Bitvector -> String
+        showIslands _ 0 = ""
+        showIslands n b = concatMap (showIslands' b) [0..5] 
+                        ++ "-- " ++ show n ++ "\n"
+                        ++ showIslands (n+1) (shiftR b 18)
+        showIslands' :: Bitvector -> Int -> String
+        showIslands' b d = concatMap (showIslands'' b d) [0..2] ++ " "
+        showIslands'' :: Bitvector -> Int -> Int -> String
+        showIslands'' b d i = if testBit b (d*3+i) then show i else "."
+
+
+-- misc trivial utility stuff ------------
+
 bitOr :: [Bitvector] -> Bitvector
 bitOr = foldl' (.|.) 0
-
 
 (.+) ::(Num t) => (t, t, t) -> (t, t, t) -> (t, t, t)
 (x, y, z) .+ (x', y', z') = (x+x', y+y', z+z')
