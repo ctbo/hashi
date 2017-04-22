7999d8cee35efa1aebcccbcb3cac8c6ac7087340
Harald Bögeholz <bo@ct.de>
2017-04-22 13:40:33 +1000

    committing forgotten files.
    These should have been in 61b7ffcf80bb04a37ccf67f1051d68d23d2b5208

---
+++ HashiBitmap.hsproj/Heredoc.hs
@@ -0,0 +1,10 @@
+module Heredoc (heredoc, fileAsString) where
+
+import Language.Haskell.TH.Quote
+import Language.Haskell.TH.Lib
+
+heredoc :: QuasiQuoter
+heredoc = QuasiQuoter (litE . stringL) (litP . stringL) (undefined) (undefined)
+
+fileAsString :: QuasiQuoter
+fileAsString = quoteFile heredoc
